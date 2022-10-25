2a660790c2898ad776f0bd8b65f34d888edf9aa4
Aniket Raj <32168046+exewhyz@users.noreply.github.com>
2022-10-25 22:07:38 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+    Copyright &copy; 2022 QuizSpot
+    </div>
+  );
+};
+
+export default Footer;
